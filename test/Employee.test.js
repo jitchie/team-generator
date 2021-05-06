a12fda8c1c25ae688a00abf9937a86b9b6cc0828
Jack Ritchie <jwf.ritchie@gmail.com>
2021-05-06 13:43:43 +1000

    third commit with half eemployee intance testinging function test

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,5 +1,26 @@
-const employee = require('./')
+const NewEmployee = require('../lib/employee')
+
+describe("Employee", () => {
+    it ('Can instantiate Employee instance', () =>{
+        const obj = new NewEmployee();
+        expect()
+    })
 // build a test to instantiate instance of Employee
+
+// Can set name via Constructor arguments
+
+//can set ID via Constructor argument
+
+//can set email via constructor argument
+
+//can get name via get Id()
+
+// can get email via getEmail()
+
+//get roll() should return "Emloyee"
+
+})
+
 
 // Can set name via Constructor arguments
 
