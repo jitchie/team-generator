e7c6d4974a28ff7c970fab6032a8cb5d3768eb85
Jack Ritchie <jwf.ritchie@gmail.com>
2021-05-11 15:18:21 +1000

    push email test not working

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,22 +1,21 @@
 const Employee = require('../lib/employee')
 
-describe("Employee", () => {
-    // build a test to instantiate instance of Employee
-    it ('Can instantiate Employee instance', () => {
-        class Employee {};                              // creates a class of employee with body {}
-        expect(new Employee()).toBeInstanceOf(Employee);// using instance matcher we are able to test is new instance of employee was invoked by class constructor 
+// build a test to instantiate instance of Employee
+test('Can instantiate Employee instance', () => {
+    class Employee {};                              // creates a class of employee with body {}
+    expect(new Employee()).toBeInstanceOf(Employee);// using instance matcher we are able to test is new instance of employee was invoked by class constructor 
     });
-    // Can set name via Constructor arguments
-    it('Can set name via class constructor arguments', () => {
-        const name = "jack";                            // create a key value pair
-        const e = new Employee(name);                   // create a new key and assign to constructor function with a parr of name
-        expect(e.name).toBe(name);                      // checks if const e matches const name using jests matcher function .toBe
+// Can set name via Constructor arguments
+test('Can set name via class constructor arguments', () => {
+    const name = "jack";                            // create a key value pair
+    const e = new Employee(name);                   // create a new key and assign to constructor function with a parr of name
+    expect(e.name).toBe(name);                      // checks if const e matches const name using jests matcher function .toBe
     });
-
-    it('Can set ID via constructor argument', () => {
+//can set email via constructor argument
+test('Can set ID via constructor argument', () => {
         const email = "jwf.ritchie@gmail.com";           //replace name with email and follow steps above
         const x = new Employee(email);
-        expect(x.email).toBe(x);
+        expect(x.email).toBe(mail);
     });
 
 //can set ID via Constructor argument
@@ -29,7 +28,7 @@ describe("Employee", () => {
 
 //get roll() should return "Emloyee"
 
-})
+
 
 
 // Can set name via Constructor arguments
