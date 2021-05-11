ac801269df5d8fe79e6ba7d74668d701899db36e
Jack Ritchie <jwf.ritchie@gmail.com>
2021-05-11 16:31:28 +1000

    got email test to work, with adding params that dont read and passing email through test, used same method on ID and it doesnt like it.

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -3,42 +3,23 @@ const Employee = require('../lib/employee')
 // build a test to instantiate instance of Employee
 test('Can instantiate Employee instance', () => {
     class Employee {};                              // creates a class of employee with body {}
-    expect(new Employee()).toBeInstanceOf(Employee);// using instance matcher we are able to test is new instance of employee was invoked by class constructor 
-    });
-// Can set name via Constructor arguments
+    expect(new Employee()).toBeInstanceOf(Employee);// using instance matcher we are able to test is new instance of employee was invoked by class constructor
+}); 
 test('Can set name via class constructor arguments', () => {
     const name = "jack";                            // create a key value pair
     const e = new Employee(name);                   // create a new key and assign to constructor function with a parr of name
     expect(e.name).toBe(name);                      // checks if const e matches const name using jests matcher function .toBe
-    });
-//can set email via constructor argument
-test('Can set ID via constructor argument', () => {
-        const email = "jwf.ritchie@gmail.com";           //replace name with email and follow steps above
-        const x = new Employee(email);
-        expect(x.email).toBe(mail);
-    });
-
-//can set ID via Constructor argument
-
-//can set email via constructor argument
-
-//can get name via get Id()
-
-// can get email via getEmail()
-
-//get roll() should return "Emloyee"
+});
+test("Can set email via constructor argument", () => {
+    const email = "jwf.ritchie@gmail.com";
+    const e = new Employee("Foo", 1, email); //need to pass in params in line with constructor even if testing just for email?
+    expect(e.email).toBe(email);
+});
+test('can set ID via constructor argument', () => {
+    const id = 86;
+    const i = new Employee(id);
+    expect(i.id).toBeInstanceOf("foo", id, email);
+});
 
 
 
-
-// Can set name via Constructor arguments
-
-//can set ID via Constructor argument
-
-//can set email via constructor argument
-
-//can get name via get Id()
-
-// can get email via getEmail()
-
-//get roll() should return "Emloyee"
