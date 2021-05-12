56a16ce7fba9d9a29f841275a16036d21a2bd35b
Jack Ritchie <jwf.ritchie@gmail.com>
2021-05-12 13:25:23 +1000

    working tests for intern

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,12 +1,22 @@
 const Employee = require('../lib/employee');
 const Intern = require('../lib/intern')
 // can set school via constructor
-test('can get school via getSchool() method', () => {
-    const schoolTest = 'USYD'
+test('can set school via the constructor arguments', () => {
+    const school = "USYD";
 
-    const s = new Employee(getSchool(schoolTest));
-    expect(s.getSchool()).toEqual('USYD');
+    const s = new Intern(null,null,null, school);
+    expect(s.school).toBe(school);
 });
 //get Roll() should return "intern"
-
+test("can return a string of 'Intern' using the getRole() method", () => {
+    const internTest = new Intern();
+    
+    expect(internTest.getRole()).toBe('Intern');
+});
 //can get school via getSchool()
+test('can set  school via constructor argument', () => {
+    const school = 'USYD'
+
+    const s = new Intern(null, null, null, school);
+    expect(s.getSchool()).toBe('USYD');
+});
