a1c80d737966053aca3c6e8c002d4acd8712a56c
Jack Ritchie <jwf.ritchie@gmail.com>
2021-05-12 17:00:54 +1000

    finsihed prompts cant get node index to fire prompt questions after installing NPM inq and req into top of index.js file

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ inquirer
         type: "input",
         message: "what's your engineer's name?",
         name: "eningeerName",
-        validate: async (input) =>{
+        validate: async (input) => {
             if(input.trim(' ') === '') {
                 return 'input required'
             } else {
@@ -27,7 +27,7 @@ inquirer
         type: "input",
         message: "what is your engineer's unique ID?",
         name: "EngineerId",
-        validate: async (input) =>{
+        validate: async (input) => {
             if(input.trim(' ') === '') {
                 return 'input required'
             } else {
@@ -39,7 +39,7 @@ inquirer
         type: "input",
         message: "what is your engineer's email?",
         name: "EngineerEmail",
-        validate: async (input) =>{
+        validate: async (input) => {
             if(input.trim(' ') === '') {
                 return 'input required'
             } else {
@@ -51,6 +51,31 @@ inquirer
         type: "input",
         message: "what is your engineer's github user name?",
         name: "EngineerGitHub",
+        validate: async (input) => {
+            if(input.trim(' ') === '') {
+                return 'input required'
+            } else {
+                return true;  
+            }
+        }
+    },
+])
+.then(engineerResponse => {
+    console.log(engineerResponse);
+})
+.catch(error => {
+    console.log(error);
+})
+};
+
+let internQuestions = () => {
+
+inquirer 
+.prompt([
+    {
+        type: "input",
+        message: "what is your Interns's name?",
+        name: "internName",
         validate: async (input) =>{
             if(input.trim(' ') === '') {
                 return 'input required'
@@ -59,43 +84,113 @@ inquirer
             }
         }
     },
-]);
-
-};
-
-let internQuestions = () => {
-
-    inquirer 
-.prompt([
     {
-//body of each question
-
+        type: "input",
+        message: "what is your interns's unique ID?",
+        name: "internId",
+        validate: async (input) =>{
+            if(input.trim(' ') === '') {
+                return 'input required'
+            } else {
+                return true;  
+            }
+        }
     },
-]);
-
+    {
+        type: "input",
+        message: "what is your interns's email?",
+        name: "internEmail",
+        validate: async (input) => {
+            if(input.trim(' ') === '') {
+                return 'input required'
+            } else {
+                return true;  
+            }
+        }
+    },
+    {
+        type: "input",
+        message: "what is your interns's school?",
+        name: "internSchool",
+        validate: async (input) => {
+            if(input.trim(' ') === '') {
+                return 'input required'
+            } else {
+                return true;  
+            }
+        }
+    },
+])
+.then(internResponse => {
+console.log(internResponse);
+})
+.catch(error =>{
+    console.log(error)
+})
 };
+
+
+
+
 let managerQuestions = () => {
-
-    inquirer 
+inquirer 
 .prompt([
     {
-//body of each question
-
+        type: "input",
+        message: "what is your manager's name?",
+        name: "managerName",
+        validate: async (input) =>{
+            if(input.trim(' ') === '') {
+                return 'input required'
+            } else {
+                return true;  
+            }
+        }
     },
-]);
+    {
+        type: "input",
+        message: "what is your managers's unique ID?",
+        name: "ManagersId",
+        validate: async (input) =>{
+            if(input.trim(' ') === '') {
+                return 'input required'
+            } else {
+                return true;  
+            }
+        }
+    },
+    {
+        type: "input",
+        message: "what is your managers's email?",
+        name: "managerEmail",
+        validate: async (input) =>{
+            if(input.trim(' ') === '') {
+                return 'input required'
+            } else {
+                return true;  
+            }
+        }
+    },
+    {
+        type: "input",
+        message: "what is your managers's office number?",
+        name: "managerOfficeNumber",
+        validate: async (input) =>{
+            if(input.trim(' ') === '') {
+                return 'input required'
+            } else {
+                return true;  
+            }
+        }
+    },
+])
+.then(managerResponse => {
+    console.log(managerResponse);
+})
+.catch(error => {
+    console.log(error)
+})
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = index
