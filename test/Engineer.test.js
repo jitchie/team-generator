55499a2ff84c0097d0d66f608ad70382b41429b3
Jack Ritchie <jwf.ritchie@gmail.com>
2021-05-12 13:39:27 +1000

    using the same methods for testing employee constructor testing, im not able to test engineers github setting

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,4 +1,11 @@
+const Engineer = require('../lib/engineer')
 //can set GitHub acc via constructor
+test('can set GitHub user name via constructor argument', () => {
+    const testGitHub = 'jitchie';
+
+    const g = new Engineer(null, null, null, GitHub);
+    expect(g.GitHub).toBe(testGitHub);
+});
 
 //get roll() shoulc return "engineer"
 
